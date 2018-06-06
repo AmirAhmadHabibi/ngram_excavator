f6d27ba74ca312697ba502f41f3e3d283b04a83b
Amirahmadhabibi <amirahmad.habibi@gmail.com>
2018-06-06 16:29:07 +0430

    added verb, noun and adj, noun search

---
+++ ngram_extractor.py
@@ -0,0 +1,245 @@
+import pickle
+
+from utilitarian import QuickDataFrame, Progresser
+from nltk.corpus import wordnet
+from nltk.stem.wordnet import WordNetLemmatizer
+
+NGRAM_PATH = 'E:/ngrams/'
+
+
+class NgramExcavator:
+    def __init__(self, processed_files=[]):
+        # read all nouns and verbs and adjectives
+        raw_nouns = set(QuickDataFrame.read_csv('./data/Nouns_ Brysbaert_2014.csv', header=False,
+                                                columns=['term', 'a', 'b'])['term'])
+        self.Lem = WordNetLemmatizer()
+        # check the nouns with wWordNet and also use the singular forms only
+        self.nouns = set()
+        not_in_wordnet = 0
+        for w in raw_nouns:
+            if len(wordnet.synsets(w)) == 0:
+                not_in_wordnet += 1
+                continue
+            s = self.Lem.lemmatize(w)
+            self.nouns.add(s)
+            # if s != w:
+            #     print(w, '->', s)
+            #     a += 1
+        print(len(raw_nouns), '->', len(self.nouns), ' | ', not_in_wordnet, 'words not in WordNet.')
+
+        self.adjectives = set(QuickDataFrame.read_csv('./data/Adjectives_Williams_1976_appendix.csv', header=False,
+                                                      columns=['term', 'a'])['term'])
+
+        verbs_r = QuickDataFrame.read_csv('./data/Verbs_Levin_1991_ch30-edited.csv', header=False,
+                                          columns=['0', '1', '2', '3', '4', '5'])
+        self.verbs = dict()
+        for i in range(len(verbs_r)):
+            for j in range(5):
+                self.verbs[verbs_r[str(j)][i]] = verbs_r['0'][i]
+
+        self.start = 1801
+        self.end = 2001
+        self.processed_files = processed_files
+        if not processed_files:
+            self.adj_nn = QuickDataFrame(columns=['year'])
+            self.vrb_nn = QuickDataFrame(columns=['year'])
+            for i in range(self.start, self.end):
+                self.adj_nn.append([i])
+                self.vrb_nn.append([i])
+            self.adj_nn.set_index(self.adj_nn['year'], unique=True)
+            self.vrb_nn.set_index(self.adj_nn['year'], unique=True)
+
+            self.adj_nn_list = dict()
+            self.vrb_nn_list = dict()
+
+        else:
+            # for adjectives
+            self.adj_nn = QuickDataFrame.read_csv('./results/adjective_noun.csv')
+            for c in self.adj_nn.cols:
+                for i in range(len(self.adj_nn)):
+                    self.adj_nn[c][i] = int(self.adj_nn[c][i])
+            self.adj_nn.set_index(self.adj_nn['year'], unique=True)
+
+            jjnn_ngrams = QuickDataFrame.read_csv('./results/adjective_noun_syntactic_ngrams.csv')
+            self.adj_nn_list = dict()
+            for i in range(len(jjnn_ngrams)):
+                self.adj_nn_list[jjnn_ngrams[i]['query']] = eval(jjnn_ngrams[i]['ngrams'])
+            # for verbs
+            self.vrb_nn = QuickDataFrame.read_csv('./results/verb_noun.csv')
+            for c in self.vrb_nn.cols:
+                for i in range(len(self.vrb_nn)):
+                    self.vrb_nn[c][i] = int(self.vrb_nn[c][i])
+            self.vrb_nn.set_index(self.vrb_nn['year'], unique=True)
+
+            vnn_ngrams = QuickDataFrame.read_csv('./results/verb_noun_syntactic_ngrams.csv')
+            self.vrb_nn_list = dict()
+            for i in range(len(vnn_ngrams)):
+                self.vrb_nn_list[vnn_ngrams[i]['query']] = eval(vnn_ngrams[i]['ngrams'])
+        print('init done!')
+
+    def read_them_all(self, target='adj', arc_type='arcs'):
+        # open each arc file and search for the combinations
+        prog = Progresser(100 - len(self.processed_files))
+        for index in range(99):
+            if index in self.processed_files:
+                continue
+            num = str(index)
+            if index < 10:
+                num = '0' + num
+            if target == 'adj':
+                with open(NGRAM_PATH + arc_type + '.' + num + '-of-99', encoding='utf-8') as infile:
+                    for line in infile:
+                        try:
+                            self._adj_liner(line)
+                        except Exception as e:
+                            print(e, ':', line)
+            elif target == 'vrb':
+                with open(NGRAM_PATH + arc_type + '.' + num + '-of-99', encoding='utf-8') as infile:
+                    for line in infile:
+                        try:
+                            self._vrb_liner(line)
+                        except Exception as e:
+                            print(e, ':', line)
+
+            self.processed_files.append(index)
+            # save every 5 files
+            if index % 5 == 0:
+                self._save_results(target, index, arc_type)
+            prog.count()
+        self._save_results(target, arc_type)
+
+    def _save_results(self, target='adj', index=99, arc_type='arcs'):
+        if target == 'adj':
+            self.adj_nn.to_csv('./results/adjective_noun-' + arc_type + '.csv')
+            jjnn_ngrams = QuickDataFrame(['query', 'ngrams'])
+            for qry, ngm in self.adj_nn_list.items():
+                jjnn_ngrams.append([qry, ngm])
+            jjnn_ngrams.to_csv('./results/adjective_noun_syntactic_ngrams-' + arc_type + '.csv')
+        elif target == 'vrb':
+            self.vrb_nn.to_csv('./results/verb_noun-' + arc_type + '.csv')
+            vnn_ngrams = QuickDataFrame(['query', 'ngrams'])
+            for qry, ngm in self.vrb_nn_list.items():
+                vnn_ngrams.append([qry, ngm])
+            vnn_ngrams.to_csv('./results/verb_noun_syntactic_ngrams-' + arc_type + '.csv')
+
+        with open('./results/' + target + '-processed_files.txt', 'w') as outfile:
+            outfile.write(str(self.processed_files))
+        print('\nsaved until', index)
+
+    def _adj_liner(self, line):
+        # each line is head_word<TAB>syntactic-ngram<TAB>total_count<TAB>counts_by_year
+        root, t, rest = line.partition('\t')
+        if root not in self.nouns:
+            return
+        s_ngram, t, rest = rest.partition('\t')
+
+        s_ngram_tokens = s_ngram.split(' ')
+        # iterate on the tokens to find an adjective and a noun that comes after it
+        for i in range(len(s_ngram_tokens)):
+            token = s_ngram_tokens[i]
+            # each syntactic-ngram is word/pos-tag/dep-label/head-index
+            try:
+                word1, pos1, dep1, head1 = token.split('/')
+            except:
+                continue
+            # look for an adjective in our list
+            if 'JJ' in pos1 and word1 in self.adjectives:
+                query = word1 + ' '
+                noun_index = int(head1) - 1
+                if noun_index < i:  # if the noun is before the adjective
+                    continue
+                try:
+                    word2, pos2, dep2, head2 = s_ngram_tokens[noun_index].split('/')
+                except:
+                    continue
+                if 'NN' in pos2:
+                    # if its plural, make it singular
+                    if 'S' in pos2:
+                        word2 = self.Lem.lemmatize(word2)
+                    if word2 in self.nouns:
+                        query += word2
+                        # if found both adjective and noun add the frequencies to the QDF
+                        self._add_to_adj_nn(query, rest, s_ngram)
+
+    def _vrb_liner(self, line):
+        # each line is head_word<TAB>syntactic-ngram<TAB>total_count<TAB>counts_by_year
+        root, t, rest = line.partition('\t')
+        if root not in self.nouns:
+            return
+        s_ngram, t, rest = rest.partition('\t')
+
+        s_ngram_tokens = s_ngram.split(' ')
+        seen_verb = False
+        # iterate on the tokens to find a verb and a noun that comes after it
+        for i in range(len(s_ngram_tokens)):
+            token = s_ngram_tokens[i]
+            # each syntactic-ngram is word/pos-tag/dep-label/head-index
+            try:
+                word, pos, dep, head = token.split('/')
+            except:
+                continue
+            if not seen_verb:
+                # look for a verb in our list
+                if 'V' in pos and word in self.verbs:
+                    query = self.verbs[word] + ' '
+                    seen_verb = True
+            else:
+                if 'NN' in pos:
+                    # if its plural, make it singular
+                    if 'S' in pos:
+                        word = self.Lem.lemmatize(word)
+                    if word in self.nouns:
+                        query += word
+                        # if found both adjective and noun add the frequencies to the QDF
+                        self._add_to_vrb_nn(query, rest, s_ngram)
+                        return
+
+    def _add_to_adj_nn(self, query, all_counts, s_ngram):
+        # adding the column for the query if it's new
+        if query not in self.adj_nn_list:
+            self.adj_nn_list[query] = set()
+            self.adj_nn.add_column(name=query, value=0)
+        self.adj_nn_list[query].add(s_ngram)
+        total_count, t, counts = all_counts.partition('\t')
+        # counts_by_year is a tab-separated list of year<comma>count items.
+        for year_count in counts.split('\t'):
+            year, t, count = year_count.partition(',')
+            if year in self.adj_nn.index:
+                self.adj_nn[query, year] = self.adj_nn[query, year] + int(count)
+
+    def _add_to_vrb_nn(self, query, all_counts, s_ngram):
+        # adding the column for the query if it's new
+        if query not in self.vrb_nn_list:
+            self.vrb_nn_list[query] = set()
+            self.vrb_nn.add_column(name=query, value=0)
+        self.vrb_nn_list[query].add(s_ngram)
+        total_count, t, counts = all_counts.partition('\t')
+        # counts_by_year is a tab-separated list of year<comma>count items.
+        for year_count in counts.split('\t'):
+            year, t, count = year_count.partition(',')
+            if year in self.vrb_nn.index:
+                self.vrb_nn[query, year] = self.vrb_nn[query, year] + int(count)
+
+
+ne = NgramExcavator(processed_files=[])
+ne.read_them_all(target='vrb', arc_type='arcs')
+
+# I have attached a sample set of adjectives and nouns, in two separate files -- you will only need the 1st column in
+# each file for extraction. Specifically, we are looking to extract the frequency raw count of (adjective, noun) phrase
+# for over the past 200 years available in Syntactic ngram.
+#
+# The way you'll do the extraction is exhaustively search for the combination of adjective+noun from the two files, and
+# collect the frequency counts over 1801--2000 only if such a pair existed during this period, and ignore the pair if it
+# did not exist. To do so, you can pair up 1 adj from the adj file and 1 noun from the noun file, e.g. "bitter_ADJ" +
+# "antelope_NOUN". Do you think this is doable? The only complication is that because nouns can be singular and plural,
+# it would be great if you can collapse the frequency raw counts by considering both of these possibilities, e.g. count
+# for "bitter_ADJ" + "antelope_NOUN" / "antelopes_NOUN" would take into account both plural and singular cases of
+# antelope.
+#
+# You can store the resulting retrieval results in a simple csv file, where each column records the frequency counts
+# over 1801--2000 for a given (adj, noun) pair, where you name that column exactly by Adjective, Noun in the title (so
+# we know which pair you extracted frequencies for).
+
+
+# The first 5 columns include all inflectional forms of the verbs, which should be collapsed in the collection of raw
+# frequency counts, over the same period as adjective, noun pairs.
---
+++ utilitarian.py
@@ -0,0 +1,421 @@
+from copy import copy
+from sys import stdout as so
+import time
+import csv
+
+
+class QuickDataFrame:
+    """A dictionary of lists
+        each column is a dictionary key
+        each row is an index in all of the lists"""
+
+    def __init__(self, columns=None):
+        if columns is None:
+            columns = []
+        self.cols = []
+        self.data = dict()
+
+        # set column names
+        unnamed_index = 0
+        for col in columns:
+            # if no name
+            if col == '' or col is None:
+                col = 'Unnamed: ' + str(unnamed_index)
+                unnamed_index += 1
+            else:
+                col = str(col)
+            # if duplicate name
+            if col in self.data:
+                col += 'I'
+            # set the name
+            self.cols.append(col)
+            self.data[col] = []
+
+        self.length = 0
+        self.index = None
+
+    def append(self, row=None, value=None):
+        """ three options for input
+            1-2. row = not None
+                    row = list  : length should be equal to number of columns
+                    row = dict  : length should be equal to number of columns
+            3. value  : puts the value for in element in the row
+
+            * appending a row would reset the index to None
+        """
+        if row is not None:
+            if len(row) != len(self.cols):
+                raise Exception('Number of items in input row must be equal to the number of columns.')
+
+            if type(row) == dict:
+                for key, val in row.items():
+                    self.data[key].append(copy(val))
+            elif type(row) == list:
+                for i in range(len(self.cols)):
+                    self.data[self.cols[i]].append(copy(row[i]))
+        else:
+            for col in self.cols:
+                self.data[col].append(copy(value))
+
+        self.length += 1
+        self.index = None
+
+    def add_column(self, name, value=None):
+        """Adds a column and fills it with None values"""
+        name = str(name)
+        if name in self.data:
+            name += 'I'
+        self.cols.append(name)
+        self.data[name] = [copy(value) for _ in range(self.length)]
+
+    def delete_column(self, name):
+        """deletes the column  if name is in columns"""
+        if name in self.data:
+            del self.data[name]
+            self.cols.remove(name)
+
+    def rename(self, columns):
+        """renames each key in the input dictionary to its value"""
+        for old, new in columns.items():
+            if old in self.cols:
+                self.data[new] = self.data.pop(old)
+                self.cols[self.cols.index(old)] = new
+        return self
+
+    def shape(self):
+        """returns the number of rows, the number of columns"""
+        return [self.length, len(self.cols)]
+
+    def rows_equal_to(self, column, value):
+        """get a QDF containing the rows in which the given column have the given value"""
+        qdf = QuickDataFrame(self.cols)
+        for i in range(self.length):
+            if self.data[column][i] == value:
+                qdf.append(self[i])
+        return qdf
+
+    def delete_rows_equal_to(self, column, value, keep_index=False):
+        bad_list = []
+        for i in range(self.length):
+            if self.data[column][i] == value:
+                bad_list.append(i)
+        removed = 0
+        for bad_index in bad_list:
+            self.delete_row(bad_index - removed, keep_index)
+            removed += 1
+
+    def row_as_dict(self, i):
+        """don't use this in large numbers. It slows you down"""
+        if self.length <= abs(i):
+            raise IndexError('index out of range')
+        if i < 0:
+            i = self.length + i
+        row = dict()
+        for col in self.cols:
+            row[col] = self.data[col][i]
+        return row
+
+    def row_as_list(self, i):
+        if self.length <= abs(i):
+            raise IndexError('index out of range')
+        if i < 0:
+            i = self.length + i
+        row = []
+        for col in self.cols:
+            row.append(self.data[col][i])
+        return row
+
+    def delete_row(self, i, keep_index=False):
+        """ deletes the ith row
+            if keep_index is False, resets the index
+            all rows after i would shift by one
+        """
+        if i < 0:
+            i = self.length + i
+        if self.length <= i:
+            raise IndexError('index out of range')
+
+        for col in self.cols:
+            del self.data[col][i]
+        self.length -= 1
+
+        if keep_index:
+            # then delete all i indices and decrease all i+k indices by 1
+            bad_keys = set()
+            for key, val in self.index.items():
+                # if index is unique
+                if type(val) == int:
+                    if val == i:
+                        bad_keys.add(key)
+                    elif val > i:
+                        self.index[key] -= 1
+                # if index is not unique
+                else:
+                    # remove i index
+                    if i in val: val.remove(i)
+                    if len(val) == 0: bad_keys.add(key)
+                    # decrease indices>i
+                    for j in range(len(val)):
+                        if val[j] > i:
+                            val[j] -= 1
+            for bk in bad_keys:
+                del self.index[bk]
+        else:
+            self.index = None
+
+    def delete_row_list(self, row_list, keep_index=False):
+        """deletes a list of rows"""
+        i_list = sorted(row_list, reverse=True)
+        for i in i_list:
+            self.delete_row(i, keep_index)
+
+    def apply(self, func, axis='columns'):
+        result = []
+        if axis == 'columns':
+            for i in range(self.length):
+                result.append(func(self.row_as_dict(i)))
+        if axis == 'rows':
+            for i in range(len(self.cols)):
+                result.append(func(self.data[self.cols[i]]))
+        return result
+
+    def set_index(self, index_list, unique=True):
+        """ if unique is True:
+                assigns a row number to each element in index_list
+
+            if unique is false:
+                assigns a list of row numbers to each element in index_list
+                that can be used like this afterwards:
+
+                i_list= qdf.index['foo']
+
+                for i in i_list:
+                    val = qdf['col'][i]
+                    ...
+
+            * index keys would be cast to str
+        """
+        if len(index_list) != self.length:
+            raise Exception('index must have equal length with the QDF')
+        self.index = dict()
+        if unique:
+            for i in range(self.length):
+                key = str(index_list[i])
+                if key in self.index:
+                    raise Exception('index values must be unique if unique=True.')
+                # TODO: what to do with indexed that are also in cols
+                # if key in self.data:
+                #     raise Exception('index values must not be in column names.')
+                self.index[key] = i
+        else:
+            for i in range(self.length):
+                key = str(index_list[i])
+                # if key in self.data:
+                #     raise Exception('index values must not be in column names.')
+                if key not in self.index:
+                    self.index[key] = []
+                self.index[key].append(i)
+
+    def index_is_unique(self):
+        if self.index is None:
+            return None
+        if type(next(iter(self.index.values()))) == list:
+            return False
+        else:
+            return True
+
+    def copy(self):
+        # TODO
+        pass
+
+    def __str__(self):
+        out_str = ''
+        # add column names
+        for i in range(len(self.cols)):
+            out_str += str(self.cols[i])
+            if i + 1 < len(self.cols):
+                out_str += ',\t'
+        out_str += '\n'
+
+        for r in range(self.length):
+            row_str = ''
+            for i in range(len(self.cols)):
+                item = str(self.data[self.cols[i]][r])
+                if ',' in item:
+                    item = '"' + item + '"'
+                row_str += item
+                if i + 1 < len(self.cols):
+                    row_str += ',\t'
+
+            if r + 1 < self.length:
+                row_str += '\n'
+
+            out_str += row_str
+        return out_str
+
+    def __getitem__(self, arg):
+        """
+            qdf[5]
+                if arg is int returns the arg'th row as a dict
+
+            qdf['foo_col'] or qdf['foo_index']
+                if arg is a str
+                    returns the column arg if arg is in column
+                    else returns the row(s) with the index if arg in index
+                        if unique one row else a new QDF with those rows
+
+            qdf['foo_col', 'foo_index']
+                if it's a tuple then uses arg[0] as column name and arg[1] as index name
+                    then if index is unique returns the one element
+                    if not, returns a list of all the elements
+
+            qdf[5:14]
+                if arg is a slice object, returns a new QDF with a copy of those rows
+
+            qdf[['col1','col2']]
+                if arg is a list, returns a new QDF with a copy of those columns
+        """
+
+        if type(arg) == int:
+            return self.row_as_dict(arg)
+
+        elif type(arg) == str:
+            # if a column return the list in that column
+            if arg in self.data:
+                return self.data[arg]
+            # if an index then return a row or a new QDF of those rows
+            elif self.index is not None and arg in self.index:
+                row_num = self.index[arg]
+                if type(row_num) == int:
+                    return self.row_as_dict(row_num)
+                else:
+                    qdf = QuickDataFrame(self.cols)
+                    for i in row_num:
+                        qdf.append(self.row_as_list(i))
+                    return qdf
+            else:
+                raise Exception('key not in column list nor index list')
+
+        elif type(arg) == tuple:
+            if self.index is None:
+                raise Exception('the index has not been set. this method of get Item needs index')
+            col, ind = arg
+            ind = str(ind)
+            if col not in self.data:
+                raise Exception('column not in column list')
+            if ind not in self.index:
+                raise Exception('Index key not in index')
+
+            row_num = self.index[ind]
+            if type(row_num) == int:
+                return self.data[col][row_num]
+            else:
+                elements = []
+                for i in row_num:
+                    elements.append(self.data[col][i])
+                return elements
+
+        elif type(arg) == slice:
+            if (arg.start is not None and self.length <= abs(arg.start)) or \
+                    (arg.stop is not None and self.length < abs(arg.stop)):
+                raise IndexError('index out of range')
+            qdf = QuickDataFrame(self.cols)
+            for i in range(*arg.indices(self.length)):
+                qdf.append(self.row_as_list(i))
+            return qdf
+
+        elif type(arg) == list:
+            qdf = QuickDataFrame()
+            for col in arg:
+                if col in self.data:
+                    qdf.add_column(col)
+                    qdf.data[col] = copy(self.data[col])
+            qdf.length = self.length
+            return qdf
+        return None
+
+    def __setitem__(self, key, value):
+        """
+            qdf['foo_col', 'foo_index'] = value
+                if index is not unique then value would be set to all those rows having the index
+
+            qdf['foo_col'] = value
+                value must be a list of the size of QDF
+        """
+        if type(key) == tuple:
+            col, ind = key
+            ind = str(ind)
+            if col not in self.data:
+                raise Exception('column not in column list')
+            if self.index is None:
+                raise Exception('the index has not been set. this method of get Item needs index')
+            if ind not in self.index:
+                raise Exception('Index key not in index')
+
+            row_num = self.index[ind]
+            if type(row_num) == int:
+                self.data[col][row_num] = value
+            else:
+                for i in row_num:
+                    self.data[col][i] = value
+        elif type(key) == str:
+            if type(value) != list or len(value) != self.length:
+                raise Exception('value must be the same size as the QDF')
+            self.data[key] = copy(value)
+
+    def __len__(self):
+        return self.length
+
+    def to_csv(self, path):
+        with open(path, 'w', encoding='utf-8', newline='') as outfile:
+            writer = csv.writer(outfile, delimiter=',')
+            writer.writerow(self.cols)
+            for r in range(self.length):
+                writer.writerow(self.row_as_list(r))
+
+    @staticmethod
+    def read_csv(path, columns=[], header=True, sep=','):
+        qdf = QuickDataFrame()
+        with open(path, 'r', encoding='utf-8') as infile:
+            first_line = True
+            for line_tokens in csv.reader(infile, delimiter=sep):
+                if first_line:
+                    if header:
+                        if columns:
+                            qdf = QuickDataFrame(columns)
+                        else:
+                            qdf = QuickDataFrame(line_tokens)
+                    else:
+                        if columns:
+                            qdf = QuickDataFrame(columns)
+                        else:
+                            qdf = QuickDataFrame(['col' + str(i) for i in range(len(line_tokens))])
+                        qdf.append(line_tokens)
+                    first_line = False
+                else:
+                    qdf.append(line_tokens)
+        return qdf
+
+
+class Progresser:
+    def __init__(self, total_num, msg=''):
+        self.total = total_num
+        self.num = 0
+        self.start_time = time.time()
+        self.msg = msg
+
+    def count(self):
+        self.show_progress(self.num)
+        self.num += 1
+
+    def show_progress(self, current_num):
+        # if current_num % 10 != 0:
+        #     return
+        eltime = time.time() - self.start_time
+        retime = (self.total - current_num - 1) * eltime / (current_num + 1)
+
+        el_str = str(int(eltime / 3600)) + ':' + str(int((eltime % 3600) / 60)) + ':' + str(int(eltime % 60))
+        re_str = str(int(retime / 3600)) + ':' + str(int((retime % 3600) / 60)) + ':' + str(int(retime % 60))
+
+        so.write('\r' + self.msg + '\ttime: ' + el_str + ' + ' + re_str
+                 + '\t\tprogress: %' + str(round(100 * (current_num + 1) / self.total, 2)) + '\t')
